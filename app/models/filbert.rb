55f91aa4168debd4e8c632fad8824b1eb845e443
hal-henningsmoen <hal.henningsmoen@gmail.com>
2013-07-15 07:38:27 -0600

    Tweak

--- app/models/filbert.rb
+++ app/models/filbert.rb
@@ -3,7 +3,7 @@ class Filbert < ActiveRecord::Base
   def self.the_sums
     group(:name).sum(:gezuntas)
   end
-
+  # NOTE:  send the data raw and RAILS / gon  converts it to correct json on the page
   def self.sums_to_json
     data = []
     the_sums.each do |key, value|
